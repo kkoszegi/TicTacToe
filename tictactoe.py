588cc93d02c7b283682c284538adb3bcc8767f9b
Molnar Endre <druendre@gmail.com>
2019-03-12 14:32:56 +0100

    a jatekosok tudnak mar rakni

--- tictactoe.py
+++ tictactoe.py
@@ -1,3 +1,5 @@
+import itertools
+
 def newgame():
     x_player = input("Enter the name of Player1: ")
     o_player = input("Enter the name of Player2: ")
@@ -6,14 +8,25 @@ def newgame():
 
     return players
 
-board = {1: " ", 2: " ", 3: " ", 4: " ", 5: " ", 6: " ", 7: " ", 8: " ", 9: " "}
-
 used_field = [] # This will hold all fields already used
 
 #used_field.append()
 
+def change_player(player, players):
+    if player == players[1]:
+        return players[0]
+    elif player == players[0]:
+        return players[1]
+
+def board_modify(player, board, field):
+    if player == players[0]:
+        board[field] = "x"
+    elif player == players[1]:
+        board[field] = "o"
+    return board
+
 # the board
-def print_board():
+def print_board(board):
     print(' _____ _____ _____')
     print('|     |     |     |')
     print('|  ' + board[1] + '  |  ' + board[2] + '  |  ' + board[3] + '  |')
@@ -24,12 +37,17 @@ def print_board():
     print('|     |     |     |')
     print('|  ' + board[7] + '  |  ' + board[8] + '  |  ' + board[9] + '  |')
     print('|_____|_____|_____|')
+    print('\n')
 
-def choise(players):
-    input(print(players[0] + "! Choose a field on the board"))
-    input(print(players[1] + "! Choose a field on the board"))
-
+def choise(player):
+    field = int(input(f"{player}! Choose a field on the board (1-9)."))
+    return field
 
 players = newgame()
-print_board()
-choise(players)
+player = players[0]
+board = {1: " ", 2: " ", 3: " ", 4: " ", 5: " ", 6: " ", 7: " ", 8: " ", 9: " "}
+while True:
+    print_board(board)
+    field = choise(player)
+    board = board_modify(player, board, field)
+    player = change_player(player, players)
